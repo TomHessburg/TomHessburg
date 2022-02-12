1708d3ff7a62a7fe358540d1963dd92a6495b776
TomHessburg <thomas.hessburg@gmail.com>
2022-02-12 10:53:31 -0500

    .

--- src/components/left/misc/Contributions.tsx
+++ src/components/left/misc/Contributions.tsx
@@ -5,6 +5,16 @@ export default function Contributions() {
   return (
     <Card>
       <h3 className="font-bold text-2xl mb-6">Contributions</h3>
+      <p className="font-light leading-7 mb-4">
+        GitHub:{" "}
+        <a
+          href="https://github.com/TomHessburg"
+          target="_blank"
+          className="text-brand font-bold hover:underline"
+        >
+          @TomHessburg
+        </a>
+      </p>
       <div className="flex justify-center">
         <GitHubCalendar username="TomHessburg" />
       </div>
