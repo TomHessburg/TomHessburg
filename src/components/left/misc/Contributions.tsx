0764d81b7fec6f6ca7d68f92f40f7eabf5467a97
TomHessburg <thomas.hessburg@gmail.com>
2022-02-12 10:51:38 -0500

    added github contributions and music

--- src/components/left/misc/Contributions.tsx
+++ src/components/left/misc/Contributions.tsx
@@ -1,9 +1,13 @@
 import Card from "../../../utilities/Card";
+import GitHubCalendar from "react-github-calendar";
 
 export default function Contributions() {
   return (
     <Card>
       <h3 className="font-bold text-2xl mb-6">Contributions</h3>
+      <div className="flex justify-center">
+        <GitHubCalendar username="TomHessburg" />
+      </div>
     </Card>
   );
 }
--- src/components/left/misc/Music.tsx
+++ src/components/left/misc/Music.tsx
@@ -1,9 +1,22 @@
 import Card from "../../../utilities/Card";
+import ReactPlayer from "react-player";
 
 export default function Music() {
   return (
     <Card>
       <h3 className="font-bold text-2xl mb-6">Music</h3>
+
+      <ReactPlayer
+        width="100%"
+        height="300px"
+        url={`https://soundcloud.com/baronfields/sets/discography?si=827d457220ac4c42a9b9335f2e1626c0&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing`}
+      />
+      <div className="h-12" />
+      <ReactPlayer
+        width="100%"
+        height="300px"
+        url={`https://soundcloud.com/baronfields/sets/pretty-words-ep-full-ep-out-8?si=eb8b01f0b34d4a5d944cfb08fa4db45a&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing`}
+      />
     </Card>
   );
 }
