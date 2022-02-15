45c8247998234b7a22ad25b0cd751a68f4344448
TomHessburg <thomas.hessburg@gmail.com>
2022-02-15 16:41:25 -0500

    .

--- src/components/right/Right.tsx
+++ src/components/right/Right.tsx
@@ -6,9 +6,6 @@ import Work from "./Work";
 export default function Right() {
   return (
     <div className="grid-span-1">
-      {/* <div className="w-full flex flex-wrap items-center justify-end mb-6 relative lg:fixed lg:top-12 lg:right-12 z-10"> */}
-      {/* <Links /> */}
-      {/* </div> */}
       <Work />
       <Projects />
       <Education />
--- src/utilities/Card.tsx
+++ src/utilities/Card.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Card: React.FC<{}> = ({ children }) => {
   return (
-    <div className="w-full bg-dusty/70 backdrop-blur p-6 mb-6">{children}</div>
+    <motion.div
+      // whileHover={{
+      //   scale: 1.03,
+      //   transition: { duration: 0.25 },
+      // }}
+      initial="hidden"
+      whileInView="visible"
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true }}
+      variants={{
+        visible: { opacity: 1, y: 0 },
+        hidden: { opacity: 0, y: 100 },
+      }}
+      className="w-full bg-dusty/70 backdrop-blur p-6 mb-6"
+    >
+      {children}
+    </motion.div>
   );
 };
 
